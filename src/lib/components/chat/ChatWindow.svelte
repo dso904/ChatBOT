912b63669d2b0333a6e94f653e2cc95f381150fe
dso904 <55314406+dso904@users.noreply.github.com>
2024-04-11 14:06:55 +0530

    brought back to old version

--- src/lib/components/chat/ChatWindow.svelte
+++ src/lib/components/chat/ChatWindow.svelte
@@ -309,7 +309,9 @@
 							<ChatInput value="Sorry, something went wrong. Please try again." disabled={true} />
 						{:else}
 							<ChatInput
-								placeholder="Ask anything"
+								placeholder={isReadOnly
+									? "This conversation is read-only. Start a new one to continue!"
+									: "Ask anything"}
 								bind:value={message}
 								on:submit={handleSubmit}
 								on:beforeinput={(ev) => {
@@ -347,6 +349,58 @@
 					</div>
 				{/if}
 			</form>
+			<div
+				class="mt-2 flex justify-between self-stretch px-1 text-xs text-gray-400/90 max-md:mb-2 max-sm:gap-2"
+			>
+				<p>
+					Model:
+					{#if !assistant}
+						{#if models.find((m) => m.id === currentModel.id)}
+							<a
+								href="{base}/settings/{currentModel.id}"
+								class="inline-flex items-center hover:underline"
+								>{currentModel.displayName}<CarbonCaretDown class="text-xxs" /></a
+							>
+						{:else}
+							<span class="inline-flex items-center line-through dark:border-gray-700">
+								{currentModel.id}
+							</span>
+						{/if}
+					{:else}
+						{@const model = models.find((m) => m.id === currentModel.id)}
+						{#if model}
+							<a
+								href="{base}/settings/assistants/{assistant._id}"
+								class="inline-flex items-center border-b hover:text-gray-600 dark:border-gray-700 dark:hover:text-gray-300"
+								>{model?.displayName}<CarbonCaretDown class="text-xxs" /></a
+							>
+						{:else}
+							<span class="inline-flex items-center line-through dark:border-gray-700">
+								{currentModel.id}
+							</span>
+						{/if}
+					{/if}
+					<span class="max-sm:hidden">·</span><br class="sm:hidden" /> Generated content may be inaccurate
+					or false.
+				</p>
+				{#if messages.length}
+					<button
+						class="flex flex-none items-center hover:text-gray-400 max-sm:rounded-lg max-sm:bg-gray-50 max-sm:px-2.5 dark:max-sm:bg-gray-800"
+						type="button"
+						class:hover:underline={!isSharedRecently}
+						on:click={onShare}
+						disabled={isSharedRecently}
+					>
+						{#if isSharedRecently}
+							<CarbonCheckmark class="text-[.6rem] sm:mr-1.5 sm:text-green-600" />
+							<div class="text-green-600 max-sm:hidden">Link copied to clipboard</div>
+						{:else}
+							<CarbonExport class="text-[.6rem] sm:mr-1.5 sm:text-primary-500" />
+							<div class="max-sm:hidden">Share this conversation</div>
+						{/if}
+					</button>
+				{/if}
+			</div>
 		</div>
 	</div>
 </div>
